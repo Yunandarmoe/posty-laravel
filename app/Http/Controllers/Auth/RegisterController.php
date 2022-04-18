b162f8f72e2760f094310c279d68b0915e85b7b9
YuNandarMoe <scm.yunandarmoe@gmail.com>
2022-04-18 21:52:00 +0630

    Login and Logout

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -16,18 +16,17 @@ class RegisterController extends Controller
 
     public function store(Request $request)
     {
-        //dd($request->only('email', 'password'));
-
+        //dd('abc');
         $this->validate($request, [
             'name' => 'required|max:255',
-            'username' => 'required|max:255',
+            //'username' => 'required|max:255',
             'email' => 'required|email|max:255',
             'password' => 'required|confirmed',
         ]);
 
         User::create([
             'name' => $request->name,
-            'username' => $request->username,
+            //'username' => $request->username,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -16,7 +16,7 @@
         @enderror
       </div>
 
-      <div class="mb-4">
+      <!--<div class="mb-4">
         <label for="username" class="sr-only">Username</label>
         <input type="text" name="username" id="username" placeholder="Your username" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('username') border-red-500 @enderror" value="{{ old('username') }}">
         @error('username')
@@ -24,7 +24,7 @@
           {{ $message }}
         </div>
         @enderror
-      </div>
+      </div>-->
 
       <div class="mb-4">
         <label for="email" class="sr-only">Email</label>
