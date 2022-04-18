42ae610133966ecc5f66130070fea2c814898fdc
YuNandarMoe <scm.yunandarmoe@gmail.com>
2022-04-18 22:42:49 +0630

    Post form

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\LoginController;
@@ -19,11 +20,13 @@ Route::post('/login', [LoginController::class, 'store']);
 Route::post('/logout', [LogoutController::class, 'store'])->name('logout');
 //Route::post('/login', [LoginController::class, 'store']);
 
-
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
 Route::post('/register', [RegisterController::class, 'store']);
 
+Route::get('/posts', [PostController::class, 'index'])->name('posts');
+Route::post('/posts', [PostController::class, 'store']);
 
-Route::get('/posts', function () {
-    return view('posts.index');
-});
+
+//Route::get('/posts', function () {
+//    return view('posts.index');
+//});
