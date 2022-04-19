36082734050119daff3bb8fa17a14a147dab5f27
YuNandarMoe <scm.yunandarmoe@gmail.com>
2022-04-19 13:54:47 +0630

    Final tidying

--- resources/views/users/posts/index.blade.php
+++ resources/views/users/posts/index.blade.php
@@ -7,16 +7,17 @@
       <h1 class="text-2xl font-medium mb-1">{{ $user->name }}</h1>
       <p>Posted {{ $posts->count() }} {{ Str::plural('post', $posts->count()) }} and received {{ $user->receivedLikes->count() }} likes</p>
     </div>
-
     <div class="bg-white p-6 rounded-lg">
       @if ($posts->count())
-      @foreach ($posts as $post)
-      <x-post :post="$post" />
-      @endforeach
+        @foreach ($posts as $post)
+        <x-post :post="$post" />
+        @endforeach
 
-      {{ $posts->links() }}
+      <div class="pagination">
+        {{ $posts->links() }}
+      </div>
       @else
-      <p>{{ $user->name }} does not have any posts</p>
+        <p>{{ $user->name }} does not have any posts</p>
       @endif
     </div>
   </div>
