9f1bf97771a4d3b3e0c5fff184a94d46669d4074
YuNandarMoe <scm.yunandarmoe@gmail.com>
2022-04-19 08:50:38 +0630

    Seeding with model factories

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Post;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,7 @@ class PostFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'body' => $this->faker->sentence(20),
         ];
     }
 }
